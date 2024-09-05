6fcb4e050d98dffdd65fe7a5960d05291b37d22b
SczSca <antonio.alvarez@encora.com>
2024-09-04 21:54:52 -0600

    add FilterNPagination and Add component

--- front-end/to-do_app/src/pages/Home/Home.tsx
+++ front-end/to-do_app/src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 //import { FormEvent } from "react";
+import { AddModal } from "../../components/AddModal/AddModal";
 import { Button } from "../../components/Button/Button";
+import { FilterNPaginationModal } from "../../components/FilterNPaginationModal/FilterNPaginationModal";
 import { ListModal } from "../../components/ListModal/ListModal";
 import { SearchModal } from "../../components/SearchModal/SearchModal";
 import "./Home.css";
@@ -7,7 +9,9 @@ export const Home = () => {
   return (
     <div className="home">
       <SearchModal />
+      <FilterNPaginationModal />
       <ListModal />
+      <AddModal />
       <div className="new__task__container">
         <Button
           className="button button__open__modal"
