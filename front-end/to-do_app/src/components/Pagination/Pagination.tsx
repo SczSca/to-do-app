7b6b8d07cf594c54b69c7341d7e024f4c51968cf
SczSca <antonio.alvarez@encora.com>
2024-09-05 16:06:11 -0600

    add buttons component

--- front-end/to-do_app/src/components/Pagination/Pagination.tsx
+++ front-end/to-do_app/src/components/Pagination/Pagination.tsx
@@ -1,6 +1,12 @@
 import {} from "react";
 import "./Pagination.css";
+import { Button } from "../Button/Button";
 
 export const Pagination = () => {
-  return <div className="pagination">Pagination</div>;
+  return (
+    <div className="pagination">
+      <Button className="button__pagination">{"<"}</Button>
+      <Button className="button__pagination">{">"}</Button>
+    </div>
+  );
 };
