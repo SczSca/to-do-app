35253a29337a2a96053cddae246d0cad962b2e98
SczSca <antonio.alvarez@encora.com>
2024-09-04 21:58:03 -0600

    add AddModal component [in progress]

--- front-end/to-do_app/src/components/AddModal/AddModal.tsx
+++ front-end/to-do_app/src/components/AddModal/AddModal.tsx
@@ -1,5 +1,19 @@
 import {} from "react";
+import "./AddModal.css";
+import { TextBox } from "../TextBox/TextBox";
 
 export const AddModal = () => {
-  return <div className="add__modal"></div>;
+  return (
+    <div className="add__modal">
+      <div className="modal__content">
+        <TextBox
+          id="name_add"
+          name="name_add"
+          label="Name:"
+          //onChange={(e) => handleChange(e)}
+          placeholder="Introduce task name"
+        />
+      </div>
+    </div>
+  );
 };
