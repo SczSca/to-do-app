51f2c4e3fc148139a77805ac6bcf52e6c9f53ced
SczSca <antonio.alvarez@encora.com>
2024-09-04 21:56:48 -0600

    add FilterNPagination component

---
+++ front-end/to-do_app/src/components/FilterNPaginationModal/FilterNPaginationModal.tsx
@@ -0,0 +1,20 @@
+import {} from "react";
+import "./FilterNPaginationModal.css";
+import Select from "../Select/Select";
+import { orderOptions } from "../../utils";
+import { Pagination } from "../Pagination/Pagination";
+
+export const FilterNPaginationModal = () => {
+  return (
+    <div className="filter__pagination__modal">
+      <Pagination />
+      <Select
+        options={orderOptions}
+        id="order"
+        name="order"
+        label="Order:"
+        //onChange={(e) => handleChange(e)}
+      />
+    </div>
+  );
+};
