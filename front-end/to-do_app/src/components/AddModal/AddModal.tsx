fde1a82ab30461ae2b54fe0d421542d95d7e75e0
SczSca <antonio.alvarez@encora.com>
2024-09-05 11:39:18 -0600

    add Textbox and Textbox props changes

--- front-end/to-do_app/src/components/AddModal/AddModal.tsx
+++ front-end/to-do_app/src/components/AddModal/AddModal.tsx
@@ -1,18 +1,38 @@
 import {} from "react";
 import "./AddModal.css";
 import { TextBox } from "../TextBox/TextBox";
+import { Button } from "../Button/Button";
 
 export const AddModal = () => {
   return (
     <div className="add__modal">
       <div className="modal__content">
-        <TextBox
-          id="name_add"
-          name="name_add"
-          label="Name:"
-          //onChange={(e) => handleChange(e)}
-          placeholder="Introduce task name"
-        />
+        <div className="textbox__container">
+          <TextBox
+            id="name_add"
+            name="name_add"
+            label="Name:"
+            type="text"
+            //onChange={(e) => handleChange(e)}
+            placeholder="Introduce task name"
+          />
+        </div>
+        <div className="calendar__time">
+          <TextBox
+            id="calendar"
+            name="calendar"
+            label="Assign deadline:"
+            type="date"
+            //onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="buttons__container">
+          <Button className="button button__large__gray" innerHTML="Set Time" />
+          <Button
+            className="button button__large__blue"
+            innerHTML="Save Changes"
+          />
+        </div>
       </div>
     </div>
   );
