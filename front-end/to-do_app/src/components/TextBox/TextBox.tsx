95e6c55166b7ae39c001cccbeeb2aea46a098e32
SczSca <antonio.alvarez@encora.com>
2024-09-05 11:40:58 -0600

    add 'type' prop

--- front-end/to-do_app/src/components/TextBox/TextBox.tsx
+++ front-end/to-do_app/src/components/TextBox/TextBox.tsx
@@ -2,16 +2,17 @@ import { InputHTMLAttributes } from "react";
 import "./TextBox.css";
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  type: string;
 }
 
 export const TextBox = (props: Props) => {
   //gets all the props and separates to use in the code
   //...rest are all the other props from InputHTMLAttributes<HTMLInputElement>
-  const { id, label, ...rest } = props;
+  const { id, label, type, ...rest } = props;
   return (
     <label htmlFor={id}>
       {label}
-      <input type="text" id={id} {...rest} />
+      <input type={type} id={id} {...rest} />
     </label>
   );
 };
