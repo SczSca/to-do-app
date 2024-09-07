e8325909c7d601bf92dc67fe419fdb1232720c13
SczSca <antonio.alvarez@encora.com>
2024-09-07 08:55:13 -0600

    Add TasksView component to display tasks

--- front-end/to-do_app/src/components/List/List.tsx
+++ front-end/to-do_app/src/components/List/List.tsx
@@ -1,10 +1,12 @@
 import {} from "react";
 import "./List.css";
 import { Pagination } from "../Pagination/Pagination";
+import { TasksView } from "../TasksView/TasksView";
 
 export const List = () => {
   return (
     <div className="list">
+      <TasksView></TasksView>
       <Pagination></Pagination>
     </div>
   );
