d88d6fd0867c05912447b19a2e768c5f03b80e05
SczSca <antonio.alvarez@encora.com>
2024-09-07 07:56:37 -0600

    Create component that display task info

---
+++ front-end/to-do_app/src/components/Task/Task.tsx
@@ -0,0 +1,25 @@
+import {} from "react";
+import "./Task.css";
+import { Actions } from "../Actions/Actions";
+
+/**
+ * TODO:
+ *  add functionality
+ *  add interaction with pagination
+ *  try not to die in the process
+ */
+export const Task = () => {
+  return (
+    <div className="task">
+      <input type="checkbox" name="done" id="" />
+      <p className="task__name">
+        Aprender programación es una habilidad importante que puede abrir muchas
+        oportunidades en diversos campos de la tecnología.
+      </p>
+      <div className="task__date">
+        <p>2024-09-03T14:30:00Z</p>
+      </div>
+      <Actions></Actions>
+    </div>
+  );
+};
