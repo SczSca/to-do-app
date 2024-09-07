26cfee2c3fbe00a82c8e19c81b460c8116143614
SczSca <antonio.alvarez@encora.com>
2024-09-06 19:32:21 -0600

    remove Pagination component and add modalContext

--- front-end/to-do_app/src/components/BottomContainer/BottomContainer.tsx
+++ front-end/to-do_app/src/components/BottomContainer/BottomContainer.tsx
@@ -1,16 +1,22 @@
-import {} from "react";
+import { useContext } from "react";
 import { Button } from "../Button/Button";
 import { Pagination } from "../Pagination/Pagination";
 import "./BottomContainer.css";
+import { modalContext } from "../../context/modalContext";
 export const BottomContainer = () => {
+  const { openModal } = useContext(modalContext);
   return (
     <div className="bottom__container">
-      <Pagination />
       <div className="buttons">
         <Button className="button__open__modal button__black">
           Time Metrics
         </Button>
-        <Button className="button__open__modal button__blue">
+        <Button
+          className="button__open__modal button__blue"
+          onClick={() => {
+            openModal();
+          }}
+        >
           Add new task
         </Button>
       </div>
