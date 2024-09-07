cd732fb5304f033541d26ea3a7b9d2c3eb8fce35
SczSca <antonio.alvarez@encora.com>
2024-09-07 12:23:50 -0600

    add method to open specific modal type

--- front-end/to-do_app/src/components/BottomContainer/BottomContainer.tsx
+++ front-end/to-do_app/src/components/BottomContainer/BottomContainer.tsx
@@ -2,17 +2,27 @@ import { useContext } from "react";
 import { Button } from "../Button/Button";
 import "./BottomContainer.css";
 import { modalContext } from "../../context/modalContext";
+import { ModalType } from "../../types";
 export const BottomContainer = () => {
-  const { openModal } = useContext(modalContext);
+  const { openModal, typeModal } = useContext(modalContext);
+
+  const { Add, Time } = ModalType;
   return (
     <div className="bottom__container">
       <div className="buttons">
-        <Button className="button__open__modal button__black">
+        <Button
+          className="button__open__modal button__black"
+          onClick={() => {
+            typeModal(Time);
+            openModal();
+          }}
+        >
           Time Metrics
         </Button>
         <Button
           className="button__open__modal button__blue"
           onClick={() => {
+            typeModal(Add);
             openModal();
           }}
         >
