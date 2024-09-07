429714ab8188f5e8b4175edfe7ed75b0727a1376
SczSca <antonio.alvarez@encora.com>
2024-09-06 19:37:52 -0600

    add ModalProvider

--- front-end/to-do_app/src/main.tsx
+++ front-end/to-do_app/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Home } from "./pages";
+import { ModalProvider } from "./context/modalContext";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <Home />
+      <ModalProvider>
+        <Home />
+      </ModalProvider>
     </StrictMode>
   );
 } else {
