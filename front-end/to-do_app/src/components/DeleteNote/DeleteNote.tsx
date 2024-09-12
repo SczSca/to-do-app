ed3a31595c41e447be37499e47be48307d720072
SczSca <antonio.alvarez@encora.com>
2024-09-11 21:20:49 -0600

    add delete request

--- front-end/to-do_app/src/components/DeleteNote/DeleteNote.tsx
+++ front-end/to-do_app/src/components/DeleteNote/DeleteNote.tsx
@@ -2,12 +2,15 @@ import { useContext } from "react";
 import { modalContext } from "../../context/modalContext";
 import { Button } from "../Button/Button";
 import "./DeleteNote.css";
+import { crudContext } from "../../context/crudContext";
 
 export const DeleteNote = () => {
   const { closeModal } = useContext(modalContext);
+  const { deleteTask, task, setTask } = useContext(crudContext);
+
   return (
     <div className="delete__content">
-      <p>Are you sure you want to delete this task?</p>
+      <p>Are you sure you want to delete the task &quot;{task.text}&quot;?</p>
       <div className="delete__cancel">
         <Button
           className="button__actions button__gray"
@@ -20,6 +23,7 @@ export const DeleteNote = () => {
         <Button
           className="button__actions button__red"
           onClick={() => {
+            deleteTask(task.id);
             closeModal();
           }}
         >
