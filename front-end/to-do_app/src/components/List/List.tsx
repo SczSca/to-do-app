c36235fa656a540b67f4af1ced577db781c60793
SczSca <antonio.alvarez@encora.com>
2024-09-11 21:25:27 -0600

    add get request

--- front-end/to-do_app/src/components/List/List.tsx
+++ front-end/to-do_app/src/components/List/List.tsx
@@ -1,13 +1,20 @@
-import {} from "react";
+import { useContext, useEffect } from "react";
 import "./List.css";
 import { Pagination } from "../Pagination/Pagination";
 import { TasksView } from "../TasksView/TasksView";
+import { crudContext } from "../../context/crudContext";
 
 export const List = () => {
+  const { getData } = useContext(crudContext);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
     <div className="list">
-      <TasksView></TasksView>
-      <Pagination></Pagination>
+      <TasksView />
+      <Pagination />
     </div>
   );
 };
