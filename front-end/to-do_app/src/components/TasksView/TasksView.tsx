5f7dbc7995dc5685e690407a5c769bd2c12fd5f7
SczSca <antonio.alvarez@encora.com>
2024-09-14 11:57:59 -0600

    remove todo comment

--- front-end/to-do_app/src/components/TasksView/TasksView.tsx
+++ front-end/to-do_app/src/components/TasksView/TasksView.tsx
@@ -4,17 +4,10 @@ import { Task } from "../Task/Task";
 import { crudContext } from "../../context/crudContext";
 import { TaskStructure } from "../../types";
 
-/**
- * TODO:
- *  add interaction with pagination
- *      show n tasks with its info and props
- *  try not to die in the process
- *
- */
-
 // props notes
 export const TasksView = () => {
   const { data } = useContext(crudContext);
+
   return (
     <div className="tasks__view">
       {/* Checks if data is an array (meaning it has tasks) to render tasks, render nothing if not */}
