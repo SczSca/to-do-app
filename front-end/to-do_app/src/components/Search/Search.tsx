e030361bb2b4b05043f620b661a4bd3a33c0f0b8
SczSca <antonio.alvarez@encora.com>
2024-09-14 11:54:32 -0600

    handleSubmit function to get req. Adjust elements name to the TaskRequest interface. add type submit to search btn.

--- front-end/to-do_app/src/components/Search/Search.tsx
+++ front-end/to-do_app/src/components/Search/Search.tsx
@@ -1,9 +1,17 @@
-import { ChangeEvent, DetailedHTMLProps, FormEvent, useState } from "react";
+import {
+  ChangeEvent,
+  DetailedHTMLProps,
+  FormEvent,
+  useContext,
+  useState,
+} from "react";
 import { TextBox } from "../TextBox/TextBox";
 import Select from "../Select/Select";
 import { priorityOptions, statusOptions } from "../../utils";
 import "./Search.css";
 import { Button } from "../Button/Button";
+import { TaskRequest } from "../../types";
+import { crudContext } from "../../context/crudContext";
 
 type Props = DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
@@ -11,29 +19,42 @@ type Props = DetailedHTMLProps<
 >;
 
 export const Search = (props: Props) => {
-  const [form, setForm] = useState({
-    name: "",
-    priority: "",
-    state: "",
-  });
+  const { getData } = useContext(crudContext);
 
-  const handleChange = (
-    event: ChangeEvent<HTMLSelectElement | HTMLInputElement>
-  ) => {
-    setForm((prev) => {
-      return { ...prev, [event.target.name]: event.target.value };
-    });
-  };
+  // const handleChange = (
+  //   event: ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  // ) => {
+  //   setForm((prev) => {
+  //     return { ...prev, [event.target.name]: event.target.value };
+  //   });
+  // };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formElements = new FormData(form);
+    const formData = Object.fromEntries(formElements) as unknown as TaskRequest;
+
+    const text: string = formData.taskText;
+    //Set value used in backend to let it know it is blank
+    formData.taskText = text == "" ? "blankTask_0X0" : text;
+
+    await getData(formData);
+
+    // Acceder al input directamente desde el formulario
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const taskTextInput = form.querySelector<HTMLInputElement>(
+      'input[name="taskText"]'
+    )!;
+
+    taskTextInput.value = "";
   };
   return (
     <div className="search">
       <form action="" onSubmit={handleSubmit}>
         <TextBox
           id="nameSearch"
-          name="nameSearch"
+          name="taskText"
           label="Name:"
           type="text"
           //onChange={(e) => handleChange(e)}
@@ -49,12 +70,14 @@ export const Search = (props: Props) => {
           />
           <Select
             options={statusOptions}
-            id="state"
-            name="state"
+            id="status"
+            name="status"
             label="State:"
             //onChange={(e) => handleChange(e)}
           />
-          <Button className="button">Search</Button>
+          <Button className="button" type="submit">
+            Search
+          </Button>
         </div>
       </form>
     </div>
