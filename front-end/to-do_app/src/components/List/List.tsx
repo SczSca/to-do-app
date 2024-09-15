762f853fe029f0940dea158e7a0f496e00e5d42f
SczSca <antonio.alvarez@encora.com>
2024-09-15 14:53:59 -0600

    import values and methods from crudContext for Pagination usages. Set Pagination props

--- front-end/to-do_app/src/components/List/List.tsx
+++ front-end/to-do_app/src/components/List/List.tsx
@@ -1,17 +1,25 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import "./List.css";
 import { Pagination } from "../Pagination/Pagination";
 import { TasksView } from "../TasksView/TasksView";
 import { crudContext } from "../../context/crudContext";
 
 export const List = () => {
-  const { getData } = useContext(crudContext);
-
+  const { getData, setCurrentPage, currentPage, totalPages } =
+    useContext(crudContext);
 
   return (
     <div className="list">
       <TasksView />
-      <Pagination />
+      <Pagination
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onPageChange={async (page) => {
+          setCurrentPage(page);
+          await getData(page);
+        }}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
