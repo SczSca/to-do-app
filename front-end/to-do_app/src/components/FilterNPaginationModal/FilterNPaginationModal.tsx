c5a4c35651b55538b66fe06bd01b49b3bbf468ff
SczSca <antonio.alvarez@encora.com>
2024-09-16 08:23:10 -0600

    import date and prior order var and func. add useRef to track the first render of the component to avoid calling getData inside useEffect. add handleOnClick method to switch between order val

--- front-end/to-do_app/src/components/FilterNPaginationModal/FilterNPaginationModal.tsx
+++ front-end/to-do_app/src/components/FilterNPaginationModal/FilterNPaginationModal.tsx
@@ -5,6 +5,24 @@ import { Button } from "../Button/Button";
 import { order } from "../../types";
 
 export const FilterNPaginationModal = () => {
+  const { getData, setDateOrder, setPriorOrder, priorOrder, dateOrder } =
+    useContext(crudContext);
+  const isFirstRender = useRef(true); // Track if it's the first render
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [priorOrder, dateOrder]);
+  const handleOnClick = (
+    setFunc: React.Dispatch<React.SetStateAction<order>>
+  ) => {
+    setFunc((prevOrder) => (prevOrder === order.Asc ? order.Desc : order.Asc));
+  };
   return (
     <div className="filter__pagination__modal">
       <Select
