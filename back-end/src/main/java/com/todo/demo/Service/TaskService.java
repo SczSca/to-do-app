a6a9775fa7f583aa6950b9a7ef9e7ade12e2266e
SczSca <antonio.alvarez@encora.com>
2024-09-16 08:43:17 -0600

    adjust calls of findByCriteria in getTimeMetrics() method. Adjust getTasks() return type and findByCriteriaPagination

--- back-end/src/main/java/com/todo/demo/Service/TaskService.java
+++ back-end/src/main/java/com/todo/demo/Service/TaskService.java
@@ -2,6 +2,7 @@ package com.todo.demo.Service;
 import com.todo.demo.Entity.Task;
 import com.todo.demo.Exception.ApiRequestException;
 import com.todo.demo.Model.dto.TaskDTO;
+import com.todo.demo.Model.dto.TasksPageResultDTO;
 import com.todo.demo.Model.dto.TimeDTO;
 import com.todo.demo.Model.request.TaskRequest;
 import com.todo.demo.Repository.TaskRepository;
@@ -70,23 +71,23 @@ public class TaskService {
 //        return new ResponseEntity<String>("Done status was not updated, not found!", HttpStatus.BAD_REQUEST);
     }
 
-    public ResponseEntity<List<Task>> getTasks(TaskRequest taskRequest){
-        List<Task> tasks = taskRepository.findByCriteria(taskRequest);
-        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    public ResponseEntity<TasksPageResultDTO> getTasks(TaskRequest taskRequest, int page, boolean isDateAsc, boolean isPriorAsc){
+        TasksPageResultDTO paginationResponse = taskRepository.findByCriteriaPagination(taskRequest, page, isDateAsc, isPriorAsc);
+        return new ResponseEntity<>(paginationResponse, HttpStatus.OK);
     }
 
     public ResponseEntity<TimeDTO> getTimeMetrics(){
         TimeDTO time = new TimeDTO();
-        List<Task> tasks = taskRepository.findByCriteria(new TaskRequest("","All","Done"));
+        List<Task> tasks = taskRepository.findByCriteria(new TaskRequest("","All","Completed"));
         time.setAverageTime(averageTime(tasks));
 
-        List<Task> lowPriorTasks = taskRepository.findByCriteria(new TaskRequest("", "Low", "Done"));
+        List<Task> lowPriorTasks = taskRepository.findByCriteria(new TaskRequest("", "Low", "Completed"));
         time.setLowPriorTime(averageTime(lowPriorTasks));
 
-        List<Task> mediumPriorTasks = taskRepository.findByCriteria(new TaskRequest("", "Medium", "Done"));
+        List<Task> mediumPriorTasks = taskRepository.findByCriteria(new TaskRequest("", "Medium", "Completed"));
         time.setMediumPriorTime(averageTime(mediumPriorTasks));
 
-        List<Task> highPriorTasks = taskRepository.findByCriteria(new TaskRequest("", "High", "Done"));
+        List<Task> highPriorTasks = taskRepository.findByCriteria(new TaskRequest("", "High", "Completed"));
         time.setHighPriorTime(averageTime(highPriorTasks));
 
         return new ResponseEntity<TimeDTO>(time,HttpStatus.CREATED);
@@ -100,14 +101,14 @@ public class TaskService {
         int amountTasks = tasks.size();
 
         for(Task task: tasks){
-
-        seconds += ChronoUnit.SECONDS.between(task.getCreationDate(), task.getDoneDate());
+            seconds += ChronoUnit.SECONDS.between(task.getCreationDate(), task.getDoneDate());
         }
 
         long averageSeconds = seconds/amountTasks;
         long hours = averageSeconds / 3600;
         long minutes = (averageSeconds % 3600) / 60;
         seconds = averageSeconds % 60;
+
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
