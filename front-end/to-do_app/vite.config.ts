8f1bff7f2d434db35de730d5196aa4238c28ba0b
SczSca <antonio.alvarez@encora.com>
2024-09-16 21:43:30 -0600

    adjust config for testing

--- front-end/to-do_app/vite.config.ts
+++ front-end/to-do_app/vite.config.ts
@@ -1,10 +1,18 @@
+/// <reference types="vitest"/>
+/// <reference types="Vite/client"/>
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/1
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080,
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setupTests.tsx",
   },
 });
