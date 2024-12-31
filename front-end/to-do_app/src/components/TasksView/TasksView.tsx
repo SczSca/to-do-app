3a183d9332cdd8cdf1adf188c2ac89fb19891a5b
SczSca <antonio.alvarez@encora.com>
2024-12-31 08:31:53 -0600

    update to refactored states

--- front-end/to-do_app/src/components/TasksView/TasksView.tsx
+++ front-end/to-do_app/src/components/TasksView/TasksView.tsx
@@ -6,13 +6,15 @@ import { TaskStructure } from "../../types";
 
 // props notes
 export const TasksView = () => {
-  const { data } = useContext(crudContext);
+  const { allData } = useContext(crudContext);
 
   return (
     <div className="tasks__view">
       {/* Checks if data is an array (meaning it has tasks) to render tasks, render nothing if not */}
-      {Array.isArray(data) &&
-        data.map((item: TaskStructure) => <Task key={item.id} item={item} />)}
+      {Array.isArray(allData.data) &&
+        allData.data.map((item: TaskStructure) => (
+          <Task key={item.id} item={item} />
+        ))}
     </div>
   );
 };
