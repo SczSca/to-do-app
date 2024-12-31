894dd8896b8761543d4c79f63af4c5a8c7b56f76
SczSca <antonio.alvarez@encora.com>
2024-12-31 08:22:22 -0600

    add jsdoc and inline comments

--- front-end/to-do_app/src/service/ApiService.ts
+++ front-end/to-do_app/src/service/ApiService.ts
@@ -14,6 +14,13 @@ const headers = {
 };
 const api = secureFetch(env.backendUrl, headers);
 
+/**
+ * Creates a new task.
+ *
+ * @param {TaskElements} task - The task data to create.
+ * @param {Record<string, string | number | boolean | null | undefined>} queryParams - Additional query parameters.
+ * @returns {Promise<Response>} The response from the server.
+ */
 export const createTask = async (
   task: TaskElements,
   queryParams: Record<string, string | number | boolean | null | undefined> = {}
@@ -35,6 +42,13 @@ export const createTask = async (
   }
 };
 
+/**
+ * Updates an existing task.
+ *
+ * @param {TaskElements} task - The task data to update.
+ * @param {Record<string, string | number | boolean | null | undefined>} queryParams - Additional query parameters.
+ * @returns {Promise<Response>} The response from the server.
+ */
 export const updateTask = async (
   task: TaskElements,
   queryParams: Record<string, string | number | boolean | null | undefined> = {}
@@ -60,6 +74,13 @@ export const updateTask = async (
   }
 };
 
+/**
+ * Updates the status of a task.
+ *
+ * @param {number} id - The ID of the task to update.
+ * @param {Record<string, string | number | boolean | null | undefined>} queryParams - Additional query parameters.
+ * @returns {Promise<Response>} The response from the server.
+ */
 export const patchStatusTask = async (
   id: number,
   queryParams: Record<string, string | number | boolean | null | undefined> = {}
@@ -84,6 +105,13 @@ export const patchStatusTask = async (
   }
 };
 
+/**
+ * Deletes a task.
+ *
+ * @param {number} id - The ID of the task to delete.
+ * @param {Record<string, string | number | boolean | null | undefined>} queryParams - Additional query parameters.
+ * @returns {Promise<Response>} The response from the server.
+ */
 export const deleteTask = async (
   id: number,
   queryParams: Record<string, string | number | boolean | null | undefined> = {}
@@ -102,6 +130,16 @@ export const deleteTask = async (
   }
 };
 
+/**
+ * Fetches tasks based on search parameters.
+ *
+ * @param {TaskRequest} taskRequest - The task request parameters.
+ * @param {number} page - The page number to fetch.
+ * @param {string} dateOrder - The order of the date.
+ * @param {string} priorOrder - The order of the priority.
+ * @param {Record<string, string | number | boolean | null | undefined>} queryParams - Additional query parameters.
+ * @returns {Promise<TasksPageResult>} The result of the tasks page.
+ */
 export const getData = async (
   taskRequest: TaskRequest,
   page: number,
@@ -129,6 +167,12 @@ export const getData = async (
   }
 };
 
+/**
+ * Fetches task metrics.
+ *
+ * @param {Record<string, string | number | boolean | null | undefined>} queryParams - Additional query parameters.
+ * @returns {Promise<TaskTimes>} The task times metrics.
+ */
 export const getMetrics = async (
   queryParams: Record<string, string | number | boolean | null | undefined> = {}
 ): Promise<TaskTimes> => {
@@ -142,11 +186,19 @@ export const getMetrics = async (
   }
 };
 
+/**
+ * Fetches tasks and updates the state with the retrieved tasks.
+ *
+ * @param {SearchParams} searchParams - The current search parameters.
+ * @param {React.Dispatch<React.SetStateAction<AllData>>} setAllData - Function to update the state with all data.
+ * @param {React.Dispatch<React.SetStateAction<SearchParams>>} setSearchParams - Function to update the search parameters.
+ * @param {number} [page=searchParams.currentPage] - The page number to fetch.
+ */
 export const fetchTasks = async (
   searchParams: SearchParams,
   setAllData: React.Dispatch<React.SetStateAction<AllData>>,
   setSearchParams: React.Dispatch<React.SetStateAction<SearchParams>>,
-  page: number = searchParams.currentPage //is this possible/
+  page: number = searchParams.currentPage
 ) => {
   try {
     const responseData = await getData(
@@ -156,10 +208,12 @@ export const fetchTasks = async (
       searchParams.priorOrder
     );
 
+    // Update the state with the retrieved tasks
     setAllData((prevState) => ({
       ...prevState,
       data: responseData.tasksFromPage,
     }));
+    // Update the search parameters with the new page and total pages
     setSearchParams((prevState) => ({
       ...prevState,
       totalPages: responseData.totalPages,
@@ -171,6 +225,14 @@ export const fetchTasks = async (
   }
 };
 
+/**
+ * Creates a new task and fetches the updated list of tasks.
+ *
+ * @param {TaskElements} task - The task data to create.
+ * @param {SearchParams} searchParams - The current search parameters.
+ * @param {React.Dispatch<React.SetStateAction<AllData>>} setAllData - Function to update the state with all data.
+ * @param {React.Dispatch<React.SetStateAction<SearchParams>>} setSearchParams - Function to update the search parameters.
+ */
 export const createTaskAndFetchAll = async (
   task: TaskElements,
   searchParams: SearchParams,
@@ -178,14 +240,22 @@ export const createTaskAndFetchAll = async (
   setSearchParams: React.Dispatch<React.SetStateAction<SearchParams>>
 ) => {
   try {
-    await createTask(task);
-    await fetchTasks(searchParams, setAllData, setSearchParams);
+    await createTask(task); // Create the new task
+    await fetchTasks(searchParams, setAllData, setSearchParams); // Fetch the updated list of tasks
   } catch (error) {
     console.error("Error in fetchTaskCreation:", error);
     throw error;
   }
 };
 
+/**
+ * Updates an existing task and fetches the updated list of tasks.
+ *
+ * @param {TaskElements} task - The task data to update.
+ * @param {SearchParams} searchParams - The current search parameters.
+ * @param {React.Dispatch<React.SetStateAction<AllData>>} setAllData - Function to update the state with all data.
+ * @param {React.Dispatch<React.SetStateAction<SearchParams>>} setSearchParams - Function to update the search parameters.
+ */
 export const updateTaskAndFetchAll = async (
   task: TaskElements,
   searchParams: SearchParams,
@@ -193,8 +263,8 @@ export const updateTaskAndFetchAll = async (
   setSearchParams: React.Dispatch<React.SetStateAction<SearchParams>>
 ) => {
   try {
-    await updateTask(task);
-    await fetchTasks(searchParams, setAllData, setSearchParams);
+    await updateTask(task); // Update the task
+    await fetchTasks(searchParams, setAllData, setSearchParams); // Fetch the updated list of tasks
 
     // Clear the task object after updating
     setAllData((prevState) => ({
@@ -207,6 +277,14 @@ export const updateTaskAndFetchAll = async (
   }
 };
 
+/**
+ * Deletes a task and fetches the updated list of tasks.
+ *
+ * @param {number} id - The ID of the task to delete.
+ * @param {SearchParams} searchParams - The current search parameters.
+ * @param {React.Dispatch<React.SetStateAction<AllData>>} setAllData - Function to update the state with all data.
+ * @param {React.Dispatch<React.SetStateAction<SearchParams>>} setSearchParams - Function to update the search parameters.
+ */
 export const deleteTaskAndFetchAll = async (
   id: number,
   searchParams: SearchParams,
@@ -214,8 +292,8 @@ export const deleteTaskAndFetchAll = async (
   setSearchParams: React.Dispatch<React.SetStateAction<SearchParams>>
 ) => {
   try {
-    await deleteTask(id);
-    await fetchTasks(searchParams, setAllData, setSearchParams);
+    await deleteTask(id); // Delete the task
+    await fetchTasks(searchParams, setAllData, setSearchParams); // Fetch the updated list of tasks
 
     // Clear the task object after updating
     setAllData((prevState) => ({
@@ -228,12 +306,18 @@ export const deleteTaskAndFetchAll = async (
   }
 };
 
+/**
+ * Fetches metrics and updates the state with the retrieved metrics.
+ *
+ * @param {React.Dispatch<React.SetStateAction<AllData>>} setAllData - Function to update the state with all data.
+ */
 export const fetchMetrics = async (
   setAllData: React.Dispatch<React.SetStateAction<AllData>>
 ) => {
   try {
-    const timeMetrics = await getMetrics();
+    const timeMetrics = await getMetrics(); // Fetch the metrics
 
+    // Update the state with the retrieved metrics
     setAllData((prevState) => ({
       ...prevState,
       timeMetrics,
@@ -243,136 +327,3 @@ export const fetchMetrics = async (
     throw error;
   }
 };
-
-// export const apiService = {
-//   createTask: async (task: TaskElements) => {
-//     try {
-//       const response = await fetch(`${env.backendUrl}`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json",
-//         },
-//         body: JSON.stringify(task),
-//       });
-
-//       // Check if the response is OK (status code 200-299)
-//       if (!response.ok) {
-//         const errorMessage = `Failed to create task. Status: ${response.status} - ${response.statusText}`;
-//         throw new Error(errorMessage);
-//       }
-
-//       return response.json(); // Assuming the server returns a JSON response
-//     } catch (error) {
-//       console.error("Error in createTask:", error);
-//       throw error; // Re-throw to be handled by the caller
-//     }
-//   },
-
-//   updateTask: async (task: TaskElements) => {
-//     try {
-//       const response = await fetch(`${env.backendUrl}/${task.id}`, {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json",
-//         },
-//         body: JSON.stringify(task),
-//       });
-
-//       if (!response.ok) {
-//         const errorMessage = `Failed to update task. Status: ${response.status} - ${response.statusText}`;
-//         throw new Error(errorMessage);
-//       }
-
-//       return response.json();
-//     } catch (error) {
-//       console.error("Error in updateTask:", error);
-//       throw error;
-//     }
-//   },
-
-//   deleteTask: async (id: number) => {
-//     try {
-//       const response = await fetch(`${env.backendUrl}/${id}`, {
-//         method: "DELETE",
-//         headers: {
-//           Accept: "application/json",
-//         },
-//       });
-
-//       if (!response.ok) {
-//         const errorMessage = `Failed to delete task. Status: ${response.status} - ${response.statusText}`;
-//         throw new Error(errorMessage);
-//       }
-
-//       return response.json();
-//     } catch (error) {
-//       console.error("Error in deleteTask:", error);
-//       throw error;
-//     }
-//   },
-
-//   getData: async (
-//     taskRequest: TaskRequest,
-//     page: number,
-//     dateOrder: string,
-//     priorOrder: string
-//   ) => {
-//     try {
-//       const params = {
-//         prior: taskRequest.priority,
-//         status: taskRequest.status,
-//         text: taskRequest.taskText,
-//         page: page.toString(),
-//         dateOrder: dateOrder,
-//         priorOrder: priorOrder,
-//       };
-
-//       const url = new URL(`${env.backendUrl}/search`);
-//       url.search = new URLSearchParams(params).toString();
-
-//       const response = await fetch(url, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json",
-//         },
-//       });
-
-//       if (!response.ok) {
-//         const errorMessage = `Failed to fetch tasks. Status: ${response.status} - ${response.statusText}`;
-//         throw new Error(errorMessage);
-//       }
-
-//       const result = await response.json();
-//       return result;
-//     } catch (error) {
-//       console.error("Error in getData:", error);
-//       throw error;
-//     }
-//   },
-
-//   getMetrics: async () => {
-//     try {
-//       const response = await fetch(`${env.backendUrl}/time`, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json",
-//         },
-//       });
-
-//       if (!response.ok) {
-//         const errorMessage = `Failed to fetch time metrics. Status: ${response.status} - ${response.statusText}`;
-//         throw new Error(errorMessage);
-//       }
-
-//       const result = await response.json();
-//       return result;
-//     } catch (error) {
-//       console.error("Error in getMetrics:", error);
-//       throw error;
-//     }
-//   },
-// };
