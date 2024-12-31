1b010b1a204b5c7f23a735a287599e79b9f05c57
SczSca <antonio.alvarez@encora.com>
2024-12-31 08:12:17 -0600

    move useEffect with fetchTasks to Home to avoid uneccesary api calls when its the first render

--- front-end/to-do_app/src/pages/Home/Home.tsx
+++ front-end/to-do_app/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { BottomContainer } from "../../components/BottomContainer/BottomContainer";
 import { Filter } from "../../components/Filter/Filter";
 import { List } from "../../components/List/List";
@@ -6,8 +6,34 @@ import { Modal } from "../../components/Modal/Modal";
 import { Search } from "../../components/Search/Search";
 import "./Home.css";
 import { modalContext } from "../../context/modalContext";
+import { crudContext } from "../../context/crudContext";
+import { fetchTasks } from "../../service/ApiService";
 export const Home = () => {
   const { isOpen, modalType } = useContext(modalContext);
+
+  const { searchParams, setSearchParams, setAllData } = useContext(crudContext);
+
+  const isFirstRender = useRef(true); // Track if it's the first render
+
+  // Fetch tasks when the search params change
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    fetchTasks(
+      searchParams,
+      setAllData,
+      setSearchParams,
+      searchParams.currentPage
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    searchParams.priorOrder,
+    searchParams.dateOrder,
+    searchParams.taskRequest,
+  ]);
   return (
     <div className="home">
       <Search />
